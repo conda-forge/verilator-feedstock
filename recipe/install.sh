12a2464790fb21e10ef5fd29e0792ddc8c51711f
Tim Snyder <snyder.tim@gmail.com>
2022-03-25 13:34:56 +0000

    fixup! use perl instead of sed. sheesh
    (cherry picked from commit 145f062ed4dbaf2cc33a69076ab17cf6c83e52af)

--- recipe/install.sh
+++ recipe/install.sh
@@ -17,7 +17,7 @@ case "$PKG_NAME" in
 
 	# fixup the prefix in verilated.mk so that it will get munged when installing the package
 	# avoid weird BSD sed behavior of thinking the command is the backup extension by using perl
-	perl -i "s|$BUILD_PREFIX|$PREFIX|g" "$PREFIX/share/verilator/include/verilated.mk"
+	perl -i -pe "s|$BUILD_PREFIX|$PREFIX|g" "$PREFIX/share/verilator/include/verilated.mk"
 
 	set +e
 	rg -l "$BUILD_PREFIX" "$PREFIX/share/verilator"
